7523d889e6c5f476bd5dcfd1b5faa28a6e8f0f29
owl <lonelyowl@hitler.rocks>
2025-03-17 21:13:06 +0700

    add metadata

--- src/start.sh
+++ src/start.sh
@@ -4,16 +4,22 @@
 TCMALLOC="$(ldconfig -p | grep -Po "libtcmalloc.so.\d" | head -n 1)"
 export LD_PRELOAD="${TCMALLOC}"
 
-# Serve the API and don't shutdown the container
+# Conditionally add --disable-metadata if DISABLE_METADATA is true
+METADATA_FLAG=""
+if [ "$DISABLE_METADATA" == "true" ]; then
+    METADATA_FLAG="--disable-metadata"
+fi
+
+# Serve the API and don't shut down the container
 if [ "$SERVE_API_LOCALLY" == "true" ]; then
     echo "runpod-worker-comfy: Starting ComfyUI"
-    python3 /comfyui/main.py --disable-auto-launch --disable-metadata --listen &
+    python3 /comfyui/main.py --disable-auto-launch $METADATA_FLAG --listen &
 
     echo "runpod-worker-comfy: Starting RunPod Handler"
     python3 -u /rp_handler.py --rp_serve_api --rp_api_host=0.0.0.0
 else
     echo "runpod-worker-comfy: Starting ComfyUI"
-    python3 /comfyui/main.py --disable-auto-launch --disable-metadata &
+    python3 /comfyui/main.py --disable-auto-launch $METADATA_FLAG &
 
     echo "runpod-worker-comfy: Starting RunPod Handler"
     python3 -u /rp_handler.py
